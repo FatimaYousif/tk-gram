03665ce9aca5aaf460b2c4bb8791b63224024ebd
Fatima Yousif <49322171+FatimaYousif@users.noreply.github.com>
2020-12-30 01:34:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,83 @@
+let countA=0;
+// alert("double tap your fav one!");
+function like()
+{
+    document.querySelector("#heart").style.color="red";
+    document.querySelector("#count").innerHTML=++countA;
+}
+function count2()
+{
+    document.querySelector("#heart").style.color="red";
+    document.querySelector("#count").innerHTML=++countA;
+
+}
+
+let countB=0;
+// alert("double tap your fav one!");
+function like2()
+{
+    document.querySelector("#heart2").style.color="red";
+    document.querySelector("#count2").innerHTML=++countB;
+}
+function count4()
+{
+    document.querySelector("#heart2").style.color="red";
+    document.querySelector("#count2").innerHTML=++countB;
+
+}
+
+let countC=0;
+// alert("double tap your fav one!");
+function like3()
+{
+    document.querySelector("#heart3").style.color="red";
+    document.querySelector("#count3").innerHTML=++countC;
+}
+function count6()
+{
+    document.querySelector("#heart3").style.color="red";
+    document.querySelector("#count3").innerHTML=++countC;
+
+}
+
+let countD=0;
+// alert("double tap your fav one!");
+function like4()
+{
+    document.querySelector("#heart4").style.color="red";
+    document.querySelector("#count4").innerHTML=++countD;
+}
+function count8()
+{
+    document.querySelector("#heart4").style.color="red";
+    document.querySelector("#count4").innerHTML=++countD;
+
+}
+
+let countE=0;
+// alert("double tap your fav one!");
+function like5()
+{
+    document.querySelector("#heart5").style.color="red";
+    document.querySelector("#count5").innerHTML=++countE;
+}
+function count10()
+{
+    document.querySelector("#heart5").style.color="red";
+    document.querySelector("#count5").innerHTML=++countE;
+
+}
+
+let countF=0;
+// alert("double tap your fav one!");
+function like6()
+{
+    document.querySelector("#heart6").style.color="red";
+    document.querySelector("#count6").innerHTML=++countF;
+}
+function count12()
+{
+    document.querySelector("#heart6").style.color="red";
+    document.querySelector("#count6").innerHTML=++countF;
+
+}
